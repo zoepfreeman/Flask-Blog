8a3a8cb9c9377eedadc23d0f5395da9e66d627a7
Brian <bstanton773@gmail.com>
2023-01-10 15:53:38 -0600

    Setup message flashing

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm
 
 @app.route('/')
@@ -24,8 +24,13 @@ def signup():
         password = form.password.data
         print(email, username, password)
         # TODO: Check to see if there is a User with username and/or email
+        if username == 'brians':
+            flash('That user already exists', 'danger')
+            return redirect(url_for('signup'))
         # TODO: Create a new User with form data and add to database
 
+        # Flash a success message
+        flash('Thank you for signing up!', 'success')
         # Redirect back to Home
         return redirect(url_for('index'))
 
