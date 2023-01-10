a4a2a48b44e8c72a47de40b20b60f1e30d18b671
Brian <bstanton773@gmail.com>
2023-01-10 15:09:07 -0600

    Handle Form Submission - SignUp

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.forms import SignUpForm
 
 @app.route('/')
@@ -11,8 +11,22 @@ def index():
 def posts():
     return 'These are the posts!'
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
     # Create an instance of the SignUpForm
     form = SignUpForm()
+    # Check if a POST request AND data is valid
+    if form.validate_on_submit():
+        print('Form Submitted and Validated!')
+        # Get data from the form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        # TODO: Check to see if there is a User with username and/or email
+        # TODO: Create a new User with form data and add to database
+
+        # Redirect back to Home
+        return redirect(url_for('index'))
+
     return render_template('signup.html', form=form)
