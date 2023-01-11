33948a3f75902d2052f125e3b76a6a0d992c4f44
Brian <bstanton773@gmail.com>
2023-01-11 12:39:09 -0600

    Initial database and migrate setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
+# Import the Flask Class from the flask module - will be main object
 from flask import Flask
+# Import SQLAlchemy and Migrate from their modules
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+# Import the Config class from the config module - will have all of the app's configurations
+from config import Config
 
+# Create an instance of the Flask Class called app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+# Configure the app using the Config class
+app.config.from_object(Config)
+
+# Create an instance of SQLAlchemy to represent our database
+db = SQLAlchemy(app)
+# Create an instance of Migrate to represent our migration engine
+migrate = Migrate(app, db)
+
 
 # import all of the routes from the routes file into the current folder
 from . import routes
